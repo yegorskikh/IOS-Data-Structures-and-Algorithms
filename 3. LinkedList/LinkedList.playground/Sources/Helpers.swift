9857c367e810337ae9df3fa9bcae965df5daae6d
Egor Gorskikh <60622982+egorskikh@users.noreply.github.com>
2021-05-23 21:00:08 +0300

    feat: completed COMPLETED LINKEDLIST

--- 3. LinkedList/LinkedList.playground/Sources/Helpers.swift
+++ 3. LinkedList/LinkedList.playground/Sources/Helpers.swift
@@ -2,7 +2,9 @@
 // For full license & permission details, see LICENSE.markdown.
 
 public func example(of description: String, action: () -> Void) {
+    
   print("---Example of \(description)---")
   action()
   print()
+    
 }
