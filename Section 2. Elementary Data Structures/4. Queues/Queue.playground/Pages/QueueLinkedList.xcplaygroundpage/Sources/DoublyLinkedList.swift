ef78d9ebc007c8149d8f569329cc1065a5b39f48
Egor Gorskikh <egorgorskikh@gmail.com>
2021-06-06 16:57:12 +0300

    feat: improved readability

--- Section 2. Elementary Data Structures/4. Queues/Queue.playground/Pages/QueueLinkedList.xcplaygroundpage/Sources/DoublyLinkedList.swift
+++ Section 2. Elementary Data Structures/4. Queues/Queue.playground/Pages/QueueLinkedList.xcplaygroundpage/Sources/DoublyLinkedList.swift
@@ -2,105 +2,105 @@
 // For full license & permission details, see LICENSE.markdown.
 
 public class Node<T> {
-  
-  public var value: T
-  public var next: Node<T>?
-  public var previous: Node<T>?
-  
-  public init(value: T) {
-    self.value = value
-  }
+    
+    public var value: T
+    public var next: Node<T>?
+    public var previous: Node<T>?
+    
+    public init(value: T) {
+        self.value = value
+    }
 }
 
 extension Node: CustomStringConvertible {
-  
-  public var description: String {
-    String(describing: value)
-  }
+    
+    public var description: String {
+        String(describing: value)
+    }
 }
 
 public class DoublyLinkedList<T> {
-  
-  private var head: Node<T>?
-  private var tail: Node<T>?
-  
-  public init() { }
-  
-  public var isEmpty: Bool {
-    head == nil
-  }
-  
-  public var first: Node<T>? {
-    head
-  }
-  
-  public func append(_ value: T) {
-    let newNode = Node(value: value)
     
-    guard let tailNode = tail else {
-      head = newNode
-      tail = newNode
-      return
+    private var head: Node<T>?
+    private var tail: Node<T>?
+    
+    public init() { }
+    
+    public var isEmpty: Bool {
+        head == nil
     }
     
-    newNode.previous = tailNode
-    tailNode.next = newNode
-    tail = newNode
-  }
-  
-  public func remove(_ node: Node<T>) -> T {
-    let prev = node.previous
-    let next = node.next
-    
-    if let prev = prev {
-      prev.next = next
-    } else {
-      head = next
+    public var first: Node<T>? {
+        head
     }
     
-    next?.previous = prev
-    
-    if next == nil {
-      tail = prev
+    public func append(_ value: T) {
+        let newNode = Node(value: value)
+        
+        guard let tailNode = tail else {
+            head = newNode
+            tail = newNode
+            return
+        }
+        
+        newNode.previous = tailNode
+        tailNode.next = newNode
+        tail = newNode
     }
     
-    node.previous = nil
-    node.next = nil
-    
-    return node.value
-  }
+    public func remove(_ node: Node<T>) -> T {
+        let prev = node.previous
+        let next = node.next
+        
+        if let prev = prev {
+            prev.next = next
+        } else {
+            head = next
+        }
+        
+        next?.previous = prev
+        
+        if next == nil {
+            tail = prev
+        }
+        
+        node.previous = nil
+        node.next = nil
+        
+        return node.value
+    }
 }
 
 extension DoublyLinkedList: CustomStringConvertible {
-  
-  public var description: String {
-    var string = ""
-    var current = head
-    while let node = current {
-      string.append("\(node.value) -> ")
-      current = node.next
+    
+    public var description: String {
+        var string = ""
+        var current = head
+        while let node = current {
+            string.append("\(node.value) -> ")
+            current = node.next
+        }
+        return string + "end"
     }
-    return string + "end"
-  }
 }
 
 public class LinkedListIterator<T>: IteratorProtocol {
-  
-  private var current: Node<T>?
-  
-  init(node: Node<T>?) {
-    current = node
-  }
-  
-  public func next() -> Node<T>? {
-    defer { current = current?.next }
-    return current
-  }
+    
+    private var current: Node<T>?
+    
+    init(node: Node<T>?) {
+        current = node
+    }
+    
+    public func next() -> Node<T>? {
+        defer { current = current?.next }
+        return current
+    }
 }
 
 extension DoublyLinkedList: Sequence {
-  
-  public func makeIterator() -> LinkedListIterator<T> {
-    LinkedListIterator(node: head)
-  }
+    
+    public func makeIterator() -> LinkedListIterator<T> {
+        LinkedListIterator(node: head)
+    }
 }
--- Section 2. Elementary Data Structures/4. Queues/Queue.playground/Sources/Queue.swift
+++ Section 2. Elementary Data Structures/4. Queues/Queue.playground/Sources/Queue.swift
@@ -2,10 +2,11 @@
 // For full license & permission details, see LICENSE.markdown.
 
 public protocol Queue {
-  
-  associatedtype Element
-  mutating func enqueue(_ element: Element) -> Bool
-  mutating func dequeue() -> Element?
-  var isEmpty: Bool { get }
-  var peek: Element? { get }
+    
+    associatedtype Element
+    mutating func enqueue(_ element: Element) -> Bool
+    mutating func dequeue() -> Element?
+    var isEmpty: Bool { get }
+    var peek: Element? { get }
+    
 }
